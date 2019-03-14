63fd9ddf5eaea5addc7bfda37a89408fc4549c4b
wangchifu <wangchifu@gmail.com>
2019-03-14 16:15:40 +0800

    fix bug

--- resources/views/schools/b3.blade.php
+++ resources/views/schools/b3.blade.php
@@ -10,7 +10,7 @@
     <div class="row custom-div">
         <div class="col-2">
             <div class="section-div">
-                八.各年級彈性學習課程目標／核心素養與學習重點、評量
+                八.各年級彈性學習課程目標／核心素養與學習重點、評量<span class="text-info">(特推會之會議紀錄及特殊需求課程計畫請統一上傳至第12項)</span>
             </div>
         </div>
         <div class="col-10">
