bea8fff02f0b5b996744f6a5fbc0010f9d101ec7
wangchifu <wangchifu@gmail.com>
2019-06-20 07:59:39 +0800

    fix bug

--- resources/views/layouts/c9_ok.blade.php
+++ resources/views/layouts/c9_ok.blade.php
@@ -3,16 +3,16 @@
         <td>
             年級
         </td>
-        <td>
-            一年級
-        </td>
-        <td>
-            二年級
-        </td>
-        <td>
-            三年級
-        </td>
         @if($school_group==1)
+            <td>
+                一年級
+            </td>
+            <td>
+                二年級
+            </td>
+            <td>
+                三年級
+            </td>
             <td>
                 四年級
             </td>
@@ -23,48 +23,97 @@
                 六年級
             </td>
         @endif
+        @if($school_group==2)
+            <td>
+                七年級
+            </td>
+            <td>
+                八年級
+            </td>
+            <td>
+                九年級
+            </td>
+        @endif
     </tr>
     <tr valign="top">
         <td>
             彈性學習課程
         </td>
-        <td>
-            <?php $files_c9_1 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_1')); ?>
-            @if(count($files_c9_1))
-                @foreach($files_c9_1 as $v)
-                    <?php $file_path = $year->year."&".$school_code."&c9_1&".$v;$file_path=substr($file_path,0,-4); ?>
-                    <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
-                    <br>
-                @endforeach
-            @else
-                <span class="text-danger">未傳</span>
-            @endif
-        </td>
-        <td>
-            <?php $files_c9_2 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_2')); ?>
-            @if(count($files_c9_2))
-                @foreach($files_c9_2 as $v)
-                    <?php $file_path = $year->year."&".$school_code."&c9_2&".$v;$file_path=substr($file_path,0,-4); ?>
-                    <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
-                    <br>
-                @endforeach
-            @else
+        @if($school_group==2)
+            <td>
+                <?php $files_c9_1 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_7')); ?>
+                @if(count($files_c9_1))
+                    @foreach($files_c9_1 as $v)
+                        <?php $file_path = $year->year."&".$school_code."&c9_7&".$v;$file_path=substr($file_path,0,-4); ?>
+                        <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
+                        <br>
+                    @endforeach
+                @else
                     <span class="text-danger">未傳</span>
-            @endif
-        </td>
-        <td>
-            <?php $files_c9_3 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_3')); ?>
-            @if(count($files_c9_3))
-                @foreach($files_c9_3 as $v)
-                    <?php $file_path = $year->year."&".$school_code."&c9_3&".$v;$file_path=substr($file_path,0,-4); ?>
-                    <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
-                    <br>
-                @endforeach
-            @else
+                @endif
+            </td>
+            <td>
+                <?php $files_c9_2 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_8')); ?>
+                @if(count($files_c9_2))
+                    @foreach($files_c9_2 as $v)
+                        <?php $file_path = $year->year."&".$school_code."&c9_8&".$v;$file_path=substr($file_path,0,-4); ?>
+                        <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
+                        <br>
+                    @endforeach
+                @else
                     <span class="text-danger">未傳</span>
-            @endif
-        </td>
+                @endif
+            </td>
+            <td>
+                <?php $files_c9_3 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_9')); ?>
+                @if(count($files_c9_3))
+                    @foreach($files_c9_3 as $v)
+                        <?php $file_path = $year->year."&".$school_code."&c9_9&".$v;$file_path=substr($file_path,0,-4); ?>
+                        <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
+                        <br>
+                    @endforeach
+                @else
+                    <span class="text-danger">未傳</span>
+                @endif
+            </td>
+        @endif
         @if($school_group==1)
+            <td>
+                <?php $files_c9_1 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_1')); ?>
+                @if(count($files_c9_1))
+                    @foreach($files_c9_1 as $v)
+                        <?php $file_path = $year->year."&".$school_code."&c9_1&".$v;$file_path=substr($file_path,0,-4); ?>
+                        <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
+                        <br>
+                    @endforeach
+                @else
+                    <span class="text-danger">未傳</span>
+                @endif
+            </td>
+            <td>
+                <?php $files_c9_2 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_2')); ?>
+                @if(count($files_c9_2))
+                    @foreach($files_c9_2 as $v)
+                        <?php $file_path = $year->year."&".$school_code."&c9_2&".$v;$file_path=substr($file_path,0,-4); ?>
+                        <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
+                        <br>
+                    @endforeach
+                @else
+                        <span class="text-danger">未傳</span>
+                @endif
+            </td>
+            <td>
+                <?php $files_c9_3 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_3')); ?>
+                @if(count($files_c9_3))
+                    @foreach($files_c9_3 as $v)
+                        <?php $file_path = $year->year."&".$school_code."&c9_3&".$v;$file_path=substr($file_path,0,-4); ?>
+                        <a href="{{ route('file.open',$file_path) }}" target="_blank"><small><i class="fas fa-download"></i> {{ $v }}</small></a>
+                        <br>
+                    @endforeach
+                @else
+                        <span class="text-danger">未傳</span>
+                @endif
+            </td>
             <td>
                 <?php $files_c9_4 = get_files(storage_path('app/public/upload/'.$year->year.'/'.$school_code.'/c9_4')); ?>
                 @if(count($files_c9_4))
