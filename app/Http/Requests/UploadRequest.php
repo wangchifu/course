269099bf59164b864d076a5a2f3c3e6683673a05
wangchifu <wangchifu@gmail.com>
2019-06-26 11:35:59 +0800

    檔案上限改為20MB

--- app/Http/Requests/UploadRequest.php
+++ app/Http/Requests/UploadRequest.php
@@ -24,7 +24,7 @@ class UploadRequest extends FormRequest
     public function rules()
     {
         return [
-            'files.*' => 'mimes:pdf|max:10240',
+            'files.*' => 'mimes:pdf|max:20480',
         ];
     }
 }
